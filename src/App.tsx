e4dc37b75f90e930c2c8519b6fa3ede7f82c35fe
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-30 09:29:29 +0900

    3章終了

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { requests } from "./request";
+import { Row } from "./components/Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Row fetchUrl={requests.fetchNetflixOriginals} />
+    </div>
+  );
+}
+
+export default App;

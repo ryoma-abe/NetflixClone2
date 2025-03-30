291e4edb66080fd9ff540bedfd51df7daf3a5400
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-31 07:52:23 +0900

    バナー表示実装完了

--- src/components/Banner/Layout.tsx
+++ src/components/Banner/Layout.tsx
@@ -1,5 +1,22 @@
+import { useProps } from "./useProps";
+
 export const Layout = () => {
+  const { randomMovie } = useProps();
   return (
-    <div></div>
+    <header
+      className="banner-header"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="ml-8 pt-36">
+        <h1 className="text-6xl font-extrabold pb-1">ここに〇〇</h1>
+        <div className="w-[45rem] leading-[1.3] pt-4 text-sm max-w-[360px] h-[80px]">
+          ここにディスクリプションがはいります
+        </div>
+      </div>
+    </header>
   );
 };

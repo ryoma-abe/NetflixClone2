08bbf069ad0add91a47c32c286f1114feaf1136e
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-31 08:03:57 +0900

    ランダムのバナー表示完了

--- src/components/Banner/Layout.tsx
+++ src/components/Banner/Layout.tsx
@@ -2,19 +2,21 @@ import { useProps } from "./useProps";
 
 export const Layout = () => {
   const { randomMovie } = useProps();
+  console.log(randomMovie);
+
   return (
     <header
-      className="banner-header"
+      className="h-96"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}")`,
-        backgroundPosition: "center center",
+        backgroundPosition: "top center",
       }}
     >
-      <div className="ml-8 pt-36">
-        <h1 className="text-6xl font-extrabold pb-1">ここに〇〇</h1>
+      <div className="ml-8 pt-36 text-white">
+        <h1 className="text-6xl font-extrabold pb-1">{randomMovie?.name}</h1>
         <div className="w-[45rem] leading-[1.3] pt-4 text-sm max-w-[360px] h-[80px]">
-          ここにディスクリプションがはいります
+          {randomMovie?.overview}
         </div>
       </div>
     </header>
